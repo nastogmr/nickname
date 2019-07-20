6315b62bc6a48074ec967ada171587e8b994c2e0
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-07-20 22:23:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
 
 
-client.on("ready", () => {
-  let guild = client.guilds.get('547843935964364849'); // ايدي ال سيرفر
+bot.on("ready", () => {
+  let guild = bot.guilds.get('547843935964364849'); // ايدي ال سيرفر
   setInterval(function() {
     guild.members.get('595653353783033857').setNickname('كل زق').then(() => { // الايدي مالتك
       setTimeout(() => {
@@ -18,36 +18,5 @@ client.on("ready", () => {
   }, 12000); // هذا الوقت وينعاد النك نيم من جديد
 }) 
 
-const Discord = require('discord.js');
-const client = new Discord.client();
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
 
-
-const developers = ["595653353783033857"]
-const adminprefix = "!";
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**Status You   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'wat')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**Status You   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'lis')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**Status You  ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/M3roof");
-      message.channel.send(`**Status You ${argresult} **`)
-}
-});
-
-
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
