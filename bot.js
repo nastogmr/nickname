dbf758140e6ce3fac3e31c4a20e32baac8e11a6e
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-07-20 22:16:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,40 @@ bot.on("ready", () => {
       }, 6000); // هذا الوقت الي يتغير فيه لثاني مره
     });
   }, 12000); // هذا الوقت وينعاد النك نيم من جديد
-})  
+}) 
+
+
+const Discord = require('discord.js');
+const bot = new Discord.bot();
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+
+const developers = ["595653353783033857"]
+const adminprefix = "!";
+bot.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    bot.user.setGame(argresult);
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wat')) {
+  bot.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'lis')) {
+  bot.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Status You  ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    bot.user.setGame(argresult, "https://www.twitch.tv/M3roof");
+      message.channel.send(`**Status You ${argresult} **`)
+}
+});
+ 
 
 
 
