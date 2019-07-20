5c0d4540245f2171f0076b00dd3519303d7b8f07
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-07-20 16:53:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,11 @@ const bot = new Discord.Client();
 bot.on("ready", () => {
   let guild = bot.guilds.get('547843935964364849'); // ايدي ال سيرفر
   setInterval(function() {
-    guild.members.get('595653353783033857').setNickname('._.').then(() => { // الايدي مالتك
+    guild.members.get('595653353783033857').setNickname('👑._.👑').then(() => { // الايدي مالتك
       setTimeout(() => {
-        guild.members.get('595653353783033857').setNickname('+_+').then(() => { // الايدي مالتك
+        guild.members.get('595653353783033857').setNickname('👑+_+👑').then(() => { // الايدي مالتك
           setTimeout(() => {
-            guild.members.get('595653353783033857').setNickname('-_-') // الايدي مالتك
+            guild.members.get('595653353783033857').setNickname('👑-_-👑') // الايدي مالتك
           }, 3000); // هذا الوقت الي يتغير فيه لاول مره
         });
       }, 6000); // هذا الوقت الي يتغير فيه لثاني مره
